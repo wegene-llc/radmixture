078d6924019e5be2bb6737548cb01830a0061ec5
Dajun Luo <luodajun100@gmail.com>
2017-03-08 10:41:37 +0800

    format code

--- src/matrix.c
+++ src/matrix.c
@@ -15,12 +15,16 @@ Matrix* init_matrix(int row, int col, double* data) {
     */
     Matrix* m;
     int i,j;
+
     m = (Matrix *)malloc(sizeof(Matrix));
+
     m->row = row;
     m->col = col;
     m->data = (double *)malloc(row * col * sizeof(double));
+
     if (data == NULL)
         return m;
+    
     for (i = 0; i < row; i++)
     {
         for (j = 0; j < col; j++)
